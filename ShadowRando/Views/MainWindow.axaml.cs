4d432023ae6ed599075689e2419c1e1b29ba6fed
dreamsyntax <dreamsyntax@gmail.com>
2024-02-16 16:39:09 -0700

    Allow resizing and manually handle it in Layout->Weapons scrollview

--- ShadowRando/Views/MainWindow.axaml.cs
+++ ShadowRando/Views/MainWindow.axaml.cs
@@ -4,8 +4,21 @@ namespace ShadowRando.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Control_OnSizeChanged(object? sender, SizeChangedEventArgs e)
+	{
+		switch (this.Height)
+	    {
+		    case <= 750:
+			    MainView.Layout_Weapon_Border.Height = 475;
+			    break;
+		    case > 750:
+			    MainView.Layout_Weapon_Border.Height = 475 + (this.Height - 750);
+				    break;
+	    }
     }
 }
