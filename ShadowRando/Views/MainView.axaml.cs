8954a91a6f425b56cc3e34458a32c0fb671594ff
dreamsyntax <dreamsyntax@gmail.com>
2026-01-07 18:16:36 -0700

    Set version 2026.01.07

--- ShadowRando/Views/MainView.axaml.cs
+++ ShadowRando/Views/MainView.axaml.cs
@@ -276,7 +276,7 @@ public partial class MainView : UserControl
 
 	private string selectedFolderPath;
 	private bool avaloniaPreviewUI;
-	const string programVersion = "2026.01.06";
+	const string programVersion = "2026.01.07";
 	private bool programInitialized = false;
 	private bool randomizeProcessing = false;
 
